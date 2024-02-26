c6a3a94250d87e233c6c9f5f0d170c0e67359160
krish7x <krishnakumar99g@gmail.com>
2024-02-26 16:45:34 +0530

    fix: configure env

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setupCache } from "axios-cache-interceptor";
 
-const baseURL = process.env.NEXTAPI_URL || "http://localhost:8000/api"
+const baseURL = process.env.NEXT_PUBLIC_API_URL
 const instance = axios.create({
   baseURL,
 });
